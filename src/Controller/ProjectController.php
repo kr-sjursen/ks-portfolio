4fa4aad8ab604e77c5dacfb044bbe476ffd60e97
Kristian Sjursen <kr.sjursen@gmail.com>
2026-03-08 15:46:10 +0100

    Add PokéDex feature with templates and PokePHP
    Introduce a PokéDex project: add a /pokedex route and controller that uses danrovito/pokephp to fetch Pokémon data (displaying Pokémon ID 1 and a list of the first 25). Add a new Twig template and CSS for the PokéDex UI, update the projects list to link the PokéDex, update README task status, and add a VSCode cSpell setting for "pokedex". Controller and template include TODOs for URL-driven selection, full dataset pagination, and other UI improvements.

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -1,8 +1,15 @@
 <?php
 namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use PokePHP\PokeApi;
+
 class ProjectController extends AbstractController
 {
     #[Route('/lore-tracker', name: 'lore tracker')]
@@ -15,5 +22,28 @@ class ProjectController extends AbstractController
     public function notesApp(): Response
     {
         return $this->render('main/projects/notes_app.html.twig');
+    }
+
+    #[Route('/pokedex', name: 'pokedex')]
+    public function pokedex(): Response
+    {
+        // TODO: Check for pokemon ID in url, and change displayPokemon to reflect that.
+
+        $pokeApi = new PokeApi();
+        $displayPokemon = json_decode($pokeApi->pokemon(1)); // Example: Get Pokémon with ID 1
+
+        // Grab the first 25 pokemon to an array
+        // TODO: Grab the rest of the 1025 pokemon...
+        $list = [];
+        for ($i = 1; $i <= 25; $i++) {
+            $apiResponse = $pokeApi->pokemon($i);
+            $pokemon = json_decode($apiResponse);
+            $list[] = $pokemon;
+        }
+
+        return $this->render('main/projects/pokedex.html.twig', [
+            'displayPokemon' => $displayPokemon,
+            'pokemonList' => $list
+        ]);
     }
 }
